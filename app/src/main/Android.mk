8d6bdd5862eda2c604cee7d0daa7218dcc60a5c7
Torsten Grote <t@grobox.de>
2019-07-03 11:23:07 +0200

    WIP: Build with gradle in Android.mk

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -17,22 +17,33 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    commons-io:../../libs/commons-io-2.6.jar
-include $(BUILD_MULTI_PREBUILT)
-
-include $(CLEAR_VARS)
+LOCAL_MODULE := Backup
 LOCAL_PACKAGE_NAME := Backup
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := permissions_com.stevesoltys.backup.xml whitelist_com.stevesoltys.backup.xml
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
-LOCAL_STATIC_JAVA_LIBRARIES := commons-io
-LOCAL_SRC_FILES := $(call all-java-files-under, java)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_STATIC_ANDROID_LIBRARIES := \
-	androidx.preference_preference \
-	androidx.lifecycle_lifecycle-extensions
-include $(BUILD_PACKAGE)
+
+backup_root  := $(LOCAL_PATH)/../../../
+backup_dir   := app
+backup_out   := $(TARGET_COMMON_OUT_ROOT)/obj/APPS/$(LOCAL_MODULE)_intermediates
+backup_build := $(backup_root)/$(backup_dir)/build
+backup_apk   := $(backup_build)/outputs/apk/release/app-release-unsigned.apk
+
+$(backup_apk):
+	echo $(backup_root) >> /tmp/backup.txt
+	echo $(backup_dir) >> /tmp/backup.txt
+	echo $(backup_out) >> /tmp/backup.txt
+	echo $(backup_build) >> /tmp/backup.txt
+	echo $(backup_apk) >> /tmp/backup.txt
+	rm -Rf $(backup_build)
+	mkdir -p $(backup_out)
+	ln -s $(backup_out) $(backup_build)
+	echo "sdk.dir=$(ANDROID_HOME)" > $(backup_root)/local.properties
+	cd $(backup_root) && JAVA_TOOL_OPTIONS="$(JAVA_TOOL_OPTIONS) -Dfile.encoding=UTF8" ../gradlew assembleRelease
+
+LOCAL_SRC_FILES := $(backup_apk)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
